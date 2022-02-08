7aaa3d92330a84ed93a3b48ecaa328628a4b406a
Mat Cameron <cameron.mathieu@student.greenriver.edu>
2022-02-08 18:08:53 -0500

    added model directory and data file

---
+++ model/data-layer.php
@@ -0,0 +1,12 @@
+<?php
+
+function getdessert(){
+    $dessert = array('choclate' =>'chocolate bar',
+        'vanilla'=>'vanilla cream');
+    return $dessert;
+}
+
+function getColors(){
+    $colors = array('blue','yellow','red','purple');
+    return $colors;
+}
